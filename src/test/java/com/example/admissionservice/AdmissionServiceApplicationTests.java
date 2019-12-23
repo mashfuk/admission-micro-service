ab6e801770eb960f0c601995a860a06b99c00cd4
mashfuk <mashfuk@gmail.com>
2019-12-23 21:07:51 +0100

    first commit

---
+++ src/test/java/com/example/admissionservice/AdmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
